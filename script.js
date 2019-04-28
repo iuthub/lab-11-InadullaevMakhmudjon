bfa611eea219431e11344a76130f9a5afc0919f2
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-04-28 03:00:47 -0700

    Execise 3 finished

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ const elements = {
 
 
 elements.biggerbtn.onclick = ()=>{
+	if(elements.checkbox.checked){
+		elements.textarea.style.transition = '2s ease-in';
+		elements.textarea.style.fontWeight = 'bold';
+		elements.textarea.style.color = 'green';
+		elements.textarea.style.textDecoration = 'underline';
+	}else{
+		elements.textarea.style.transition = '2s ease-out';
+		elements.textarea.style.fontWeight = 'normal';
+		elements.textarea.style.color = 'black';
+		elements.textarea.style.textDecoration = 'none';
+
+	}
 	elements.textarea.style.fontSize = '24pt';
 }
